f4d93887923414dd940b754c4cc506cc4c061ce3
ANSH-RIYAL <anshr.co18@nsut.ac.in>
2020-12-12 08:27:52 +0530

    model template bla bla

--- ML-models/modelClassTemplate.py
+++ ML-models/modelClassTemplate.py
@@ -1,18 +1,21 @@
-import tensorflow as tf
-import numpy as np
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten
+import tensorflow as tf 
+import numpy as np 
+from tensorflow.keras import Sequential 
+from tensorflow.keras.layers import Dense, Conv2D, Flatten 
 from math import floor, ceil
-
 class modelToBeImported():
 	def createModel(self):
 		self.model = Sequential()
-		self.model.add(Dense(10,input_shape = (5,)))
+		self.model.add(Dense(10, input_shape = (5,), activation = None))
+		self.model.add(Dense(20, activation = None))
+		self.model.add(Dense(40, activation = None))
+		self.model.add(Dense(80, activation = None))
+		self.model.add(Dense(40, activation = None))
+		self.model.add(Dense(20, activation = None))
+		self.model.add(Dense(10, activation = None))
+		self.model.add(Dense(5, activation = None))
 		self.model.add(Dense(1, activation = "softmax"))
 
 modelObject = modelToBeImported()
 modelObject.createModel()
-
-model = modelObject.model
-
-print(model.summary())
+print(modelObject.model.summary())
