4564850b8f682e0602d994349412f83ab7e1270b
ANSH-RIYAL <anshr.co18@nsut.ac.in>
2020-12-12 06:54:59 +0530

    writing to model.py

--- ML-models/model.py
+++ ML-models/model.py
@@ -3,3 +3,11 @@ import numpy as np
 from tensorflow.keras import Sequential 
 from tensorflow.keras.layers import Dense, Conv2D, Flatten 
 from math import floor, ceil
+class modelToBeImported():
+	def createModel(self):
+		self.model.add(Dense(10, input_shape = (5,), activation = None))
+		self.model.add(Dense(20, activation = None))
+		self.model.add(Dense(40, activation = None))
+		self.model.add(Dense(20, activation = None))
+		self.model.add(Dense(10, activation = None))
+		self.model.add(Dense(1, activation = None))
