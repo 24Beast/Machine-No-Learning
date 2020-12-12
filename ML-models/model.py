8716f136db268fe4580e36ea78765e64ca872b85
ANSH-RIYAL <anshr.co18@nsut.ac.in>
2020-12-12 07:17:55 +0530

    model.py writing for conv2d, correction to conv2d

--- ML-models/model.py
+++ ML-models/model.py
@@ -5,9 +5,13 @@ from tensorflow.keras.layers import Dense, Conv2D, Flatten
 from math import floor, ceil
 class modelToBeImported():
 	def createModel(self):
-		self.model.add(Dense(10, input_shape = (5,), activation = None))
-		self.model.add(Dense(20, activation = None))
-		self.model.add(Dense(40, activation = None))
-		self.model.add(Dense(20, activation = None))
-		self.model.add(Dense(10, activation = None))
-		self.model.add(Dense(1, activation = None))
+		self.model = Sequential()
+		self.model.add(Conv2D(6, 3, (1, 1), valid, input_shape = (12,12, 3), activation = None))
+		self.model.add(Conv2D(12, 3, (1, 1), valid, activation = None))
+		self.model.add(Conv2D(24, 3, (1, 1), valid, activation = None))
+		self.model.add(Flatten())
+		self.model.add(Dense(432, activation = None))
+		self.model.add(Dense(216, activation = None))
+		self.model.add(Dense(108, activation = None))
+		self.model.add(Dense(54, activation = None))
+		self.model.add(Dense(3, activation = softmax))
