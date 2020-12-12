28c56bd4be323c52b69de8911ca17e2ec54cee27
ANSH-RIYAL <anshr.co18@nsut.ac.in>
2020-12-12 07:35:14 +0530

    fixed activation function

--- ML-models/model.py
+++ ML-models/model.py
@@ -6,12 +6,12 @@ from math import floor, ceil
 class modelToBeImported():
 	def createModel(self):
 		self.model = Sequential()
-		self.model.add(Conv2D(6, 3, (1, 1), valid, input_shape = (12,12, 3), activation = None))
-		self.model.add(Conv2D(12, 3, (1, 1), valid, activation = None))
-		self.model.add(Conv2D(24, 3, (1, 1), valid, activation = None))
+		self.model.add(Conv2D(6, 3, (1, 1), "valid", input_shape = (12,12, 3), activation = None))
+		self.model.add(Conv2D(12, 3, (1, 1), "valid", activation = None))
+		self.model.add(Conv2D(24, 3, (1, 1), "valid", activation = None))
 		self.model.add(Flatten())
 		self.model.add(Dense(432, activation = None))
 		self.model.add(Dense(216, activation = None))
 		self.model.add(Dense(108, activation = None))
 		self.model.add(Dense(54, activation = None))
-		self.model.add(Dense(3, activation = softmax))
+		self.model.add(Dense(3, activation = "softmax"))
