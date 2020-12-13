2e7b716c0b9a26e6812e55cf6502de15d261b3bc
ANSH-RIYAL <anshr.co18@nsut.ac.in>
2020-12-14 03:42:07 +0530

    added individual functions for skl models

--- ML-models/model.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf 
-import numpy as np 
-from tensorflow.keras import Sequential 
-from tensorflow.keras.layers import Dense, Conv2D, Flatten 
-from math import floor, ceil
-class modelToBeImported():
-	def createModel(self):
-		self.model = Sequential()
-		self.model.add(Dense(1, input_shape = (5,), activation = None))
